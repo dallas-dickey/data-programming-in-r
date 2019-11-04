91130e8563e146fd5b99e665443407184389a7a6
Dallas Dickey <dallas-dickey@uiowa.edu>
2019-11-03 18:22:32 -0600

    changes

--- main.r
+++ main.r
@@ -1,22 +1,31 @@
 print('start main')
 
 #required packages
-require('rvest')
+require(rvest)
 require(ggplot2)
+require(scales)
 
 #clear workspace
 rm(list = ls())
 
 #install packages if necessary
-# install.packages("rvest")
+#install.packages("rvest")
 #install.packages("ggplot2")
+#install.packages("scales")
 
 #load libraries
 library(rvest)
 library(ggplot2)
+library(scales)
 
-#steps
+#Load Data
+
 source('load-win-loss.r')
 source('load-games.r')
 source('merge-games-win-loss.r')
 
+#plot graphs
+source('AgeToWinsRegression.r')
+source('SalaryCapToWinsRegression.r')
+source('PositionSpendingToWinsRegression.r')
+source('HeadToHeadActiveBar.r')
--- merge-games-win-loss.R
+++ merge-games-win-loss.R
@@ -1,15 +1,18 @@
 print('start merge-games-win-loss')
 
-nflAllYearsWinners <- nflAllYears[, c('Team', 'Year', 'Total_Cap')]
-nflAllYearsLosers <- nflAllYearsWinners
-names(nflAllYearsLosers) <- c('Losing_Team', 'Losing_Year', 'Losing_Total_Cap')
+#Merge Winning Teams and sal
+nflAllYearsWinners <- nflAllYears[, c('Team', 'Year', 'Active')]
 names(nflAllYearsWinners) <- c('Winning_Team', 'Winning_Year', 'Winning_Total_Cap')
+gamesSalary <- merge(allGames, nflAllYearsWinners, by.x = c('Winner/tie', 'Year'), by.y = c('Winning_Team', 'Winning_Year'))
 
-merged <- merge(allGames, nflAllYearsWinners, by.x = c('Winner/tie', 'Year'), by.y = c('Winning_Team', 'Winning_Year'))
-merged <- merge(merged, nflAllYearsLosers, by.x = c('Loser/tie', 'Year'), by.y = c('Losing_Team', 'Losing_Year'))
-merged$SalaryWins <- merged$Winning_Total_Cap > merged$Losing_Total_Cap
-merged$SalaryDifference <- merged$Winning_Total_Cap - merged$Losing_Total_Cap
-expected <- subset(merged, merged$SalaryWins==TRUE)
-expected2 <- subset(merged, abs(merged$SalaryDifference) > 25000000)
-expected3 <- subset(expected2, expected2$SalaryWins==TRUE)
+nflAllYearsLosers <- nflAllYears[, c('Team', 'Year', 'Active')]
+names(nflAllYearsLosers) <- c('Losing_Team', 'Losing_Year', 'Losing_Total_Cap')
+gamesSalary <- merge(gamesSalary, nflAllYearsLosers, by.x = c('Loser/tie', 'Year'), by.y = c('Losing_Team', 'Losing_Year'))
+
+gamesSalary$SalaryWins <- gamesSalary$Winning_Total_Cap > gamesSalary$Losing_Total_Cap
+gamesSalary$SalaryDifference <- gamesSalary$Winning_Total_Cap - gamesSalary$Losing_Total_Cap
+
+#expected <- subset(gamesSalary, gamesSalary$SalaryWins==TRUE)
+#expected2 <- subset(gamesSalary, abs(gamesSalary$SalaryDifference) > 25000000)
+#expected3 <- subset(expected2, expected2$SalaryWins==TRUE)
                    
